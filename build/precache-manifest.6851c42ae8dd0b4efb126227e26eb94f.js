86c2d1a0d35d1205c9206910e900cc71515e999b
Abhishek <abhishek@TM-ND0049.local>
2021-04-14 13:10:49 +0530

    Change in Layout

---
+++ build/precache-manifest.6851c42ae8dd0b4efb126227e26eb94f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a31a0addd90f25a5340947bb4c33cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e11211e1672eeb8c41a",
+    "url": "/static/css/main.3843ae42.chunk.css"
+  },
+  {
+    "revision": "b069dfe6caacb0dc0574",
+    "url": "/static/js/2.1ad93502.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.1ad93502.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e11211e1672eeb8c41a",
+    "url": "/static/js/main.6ef61eb5.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "ac64fb193d6c199467113a209aa606d4",
+    "url": "/static/media/backgroundSecond.ac64fb19.jpg"
+  },
+  {
+    "revision": "1ac784162860416976cadd426ba8ca6c",
+    "url": "/static/media/logo-YTS.1ac78416.svg"
+  },
+  {
+    "revision": "f61ddfd6d4163cfffc7a64ef7cc354a0",
+    "url": "/static/media/posterOne.f61ddfd6.jpg"
+  },
+  {
+    "revision": "654fb6364992d437eef39f508f19a996",
+    "url": "/static/media/posterSecond.654fb636.jpg"
+  },
+  {
+    "revision": "a4af77b0b571436d4638389ead0e06af",
+    "url": "/static/media/screenshot1.a4af77b0.jpg"
+  },
+  {
+    "revision": "43813370aa4afc5d63dfb73e98c4d31b",
+    "url": "/static/media/screenshot2.43813370.jpg"
+  },
+  {
+    "revision": "ad18210642222bef66dfbcf40a7a6826",
+    "url": "/static/media/screenshot3.ad182106.jpg"
+  }
+]);
